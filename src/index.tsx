1c45b2c4b2c5f794c3132e9120bdb300b856be28
chenwei <chenwei1@teambition.com>
2019-09-30 14:00:45 +0800

    feat: 默认使用mobx单页应用 大型项目在划分多页

--- src/index.tsx
+++ src/index.tsx
@@ -3,56 +3,44 @@
 // import registerServiceWorker from './pwa/registerServiceWorker'
 // import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import React, { Component } from 'react'
-import { Provider } from 'react-redux'
+import { observer } from 'mobx-react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
 import renderRoutes, { routeItemType } from './utils/renderRoutes'
 import { createBrowserHistory } from 'history'
+import { Provider } from 'mobx-react'
 import 'moment/locale/zh-cn'
 import 'lib-flexible'
 import { Loading } from '@teambition/clarity-design'
-import { connect } from 'react-redux'
 
 import asyncComponent from './components/AsyncComponent'
 import './styles/index.styl'
-import { store } from './store'
-import { IRootState } from '@/store'
+import TodoStore from './stores/TodoStore'
+import loaderStore from '@/stores/LoaderStore'
 
 function mountApp() {
   const history = createBrowserHistory()
+  const stores = {
+    todoStore: new TodoStore(),
+  }
   const routes: routeItemType[] = [
     {
       path: '/',
-      component: asyncComponent(() => import('./pages/PC/Rematch_Todo')),
+      component: asyncComponent(() => import('./pages/PC/Home')),
     },
   ]
 
-  type IProps = ReturnType<typeof mapState>
-
-  const LoaderWapper = (props: IProps) => (
-    <Loading spinning={props.loading} color="blue">
-      <Router history={history}>{renderRoutes(routes)}</Router>
-    </Loading>
-  )
-
-  const mapState = (state: IRootState) => {
-    return {
-      loading: state.loader.loading,
-    }
-  }
-
-  const ConnectLoader = connect(
-    mapState,
-    null,
-  )(LoaderWapper)
+  @observer
   class App extends Component {
     public render() {
       return (
         // <ConfigProvider locale={zh_CN}>
         <div style={{ height: '100vh' }}>
-          <Provider store={store}>
-            <ConnectLoader />
-          </Provider>
+          <Loading spinning={loaderStore.loading} color="blue">
+            <Provider {...stores}>
+              <Router history={history}>{renderRoutes(routes)}</Router>
+            </Provider>
+          </Loading>
         </div>
         // </ConfigProvider>,
       )
--- src/rematch_index.tsx
+++ src/rematch_index.tsx
@@ -3,44 +3,56 @@
 // import registerServiceWorker from './pwa/registerServiceWorker'
 // import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import React, { Component } from 'react'
-import { observer } from 'mobx-react'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
 import renderRoutes, { routeItemType } from './utils/renderRoutes'
 import { createBrowserHistory } from 'history'
-import { Provider } from 'mobx-react'
 import 'moment/locale/zh-cn'
 import 'lib-flexible'
 import { Loading } from '@teambition/clarity-design'
+import { connect } from 'react-redux'
 
 import asyncComponent from './components/AsyncComponent'
 import './styles/index.styl'
-import TodoStore from './stores/TodoStore'
-import loaderStore from '@/stores/LoaderStore'
+import { store } from './store'
+import { IRootState } from '@/store'
 
 function mountApp() {
   const history = createBrowserHistory()
-  const stores = {
-    todoStore: new TodoStore(),
-  }
   const routes: routeItemType[] = [
     {
       path: '/',
-      component: asyncComponent(() => import('./pages/PC/Home')),
+      component: asyncComponent(() => import('./pages/PC/Rematch_Todo')),
     },
   ]
 
-  @observer
+  type IProps = ReturnType<typeof mapState>
+
+  const LoaderWapper = (props: IProps) => (
+    <Loading spinning={props.loading} color="blue">
+      <Router history={history}>{renderRoutes(routes)}</Router>
+    </Loading>
+  )
+
+  const mapState = (state: IRootState) => {
+    return {
+      loading: state.loader.loading,
+    }
+  }
+
+  const ConnectLoader = connect(
+    mapState,
+    null,
+  )(LoaderWapper)
   class App extends Component {
     public render() {
       return (
         // <ConfigProvider locale={zh_CN}>
         <div style={{ height: '100vh' }}>
-          <Loading spinning={loaderStore.loading} color="blue">
-            <Provider {...stores}>
-              <Router history={history}>{renderRoutes(routes)}</Router>
-            </Provider>
-          </Loading>
+          <Provider store={store}>
+            <ConnectLoader />
+          </Provider>
         </div>
         // </ConfigProvider>,
       )
