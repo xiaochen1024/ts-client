9acc00e40ae5096b48a4270247528d09975da885
chenwei <chenwei1@teambition.com>
2019-09-30 15:52:03 +0800

    feat: 脚手架模版

---
+++ mobile_postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require('./tools/postcss-pxtorem-exclude.js')({
+      rootValue: 35.5,
+      unitPrecision: 5,
+      propList: ['*', '!font*'],
+      selectorBlackList: [],
+      minPixelValue: 1,
+      exclude: ['@teambition', 'pages/PC', 'components/PC'],
+    }),
+    require('autoprefixer')(),
+  ],
+}
